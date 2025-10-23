03a7884cecb6eaa827328fcf757a39d5e6af89dd
aaronTibbetts <103538042+aaronTibbetts@users.noreply.github.com>
2025-10-22 20:44:37 -0700

    Simplify FIRST set printing in Task3
    Refactor Task3 to simplify FIRST set printing

--- project2.cc
+++ project2.cc
@@ -91,32 +91,10 @@ int main (int argc, char* argv[])
             break;
 
         case 3: Task3();
-        firstSets= printFirstSet(grammar);
-        order = getVector(grammar);
-        termOrder =getVectorTerm(grammar);
-
-        for(const auto& entry: order){
-                std::cout<<"FIRST(" << entry <<") = { ";
-                int count = 0;
-                for(int it = 0; it < termOrder.size(); it++){
-                    if(contains(firstSets[entry], termOrder[it])){
-                        if(firstSets[entry].size() == 1){
-                            std::cout<< termOrder[it];
-                        }else{
-                            if(count == firstSets[entry].size()-1){
-                                std::cout<<termOrder[it];
-                            }else{
-                                    std::cout<<termOrder[it]<<", ";
-                                    count++;
-                                }
-                                
-                            }
-                        } 
-                        
-                    }
-                    std::cout<<" }\n";
-
-                }
+            firstSets= firstSet(grammar);
+            order = getVector(grammar);
+            termOrder = getVectorTerm(grammar);
+            printFirstSet(firstSets);
             break;
 
         case 4: Task4();
